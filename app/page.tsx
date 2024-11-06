26017be1bf663e2a14c859fbf70d692f6e043b0c
oeleomar <eleomar.dev@gmail.com>
2024-11-06 13:08:16 -0400

    chore: Add Shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return <h1 className="py-1 text-red-500">Hello World</h1>;
+  return (
+    <>
+      <h1 className="py-1 text-red-500">Hello World</h1>
+      <Button>Click me</Button>
+    </>
+  );
 }
