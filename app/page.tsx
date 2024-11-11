486ec90032ab5875a363a94839eaead38109e73c
oeleomar <eleomar.dev@gmail.com>
2024-11-11 16:39:52 -0400

    feat: ensure transactions and subscriptions pages are protected

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "./_components/navbar";
 
-export default function Home() {
-  const { userId } = auth();
+export default async function Home() {
+  const { userId } = await auth();
 
   if (!userId) {
     redirect("/login");
