f49abd6e2b837c4838928a8fd9c4bd51a75476fc
oeleomar <eleomar.dev@gmail.com>
2024-11-13 16:53:48 -0400

    feat: add expenses per category component

--- app/(home)/_components/expenses-per-category.tsx
+++ app/(home)/_components/expenses-per-category.tsx
@@ -1,6 +1,7 @@
 import { CardContent, CardHeader, CardTitle } from "@/app/_components/ui/card";
 import { Progress } from "@/app/_components/ui/progress";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import { TRANSACTION_CATEGORY_LABEL } from "@/app/_constants/transaction";
 import { TotalExpensePerCategory } from "@/app/_data/get-dashboard/types";
 
 interface ExpensesPerCategoryProps {
@@ -11,15 +12,17 @@ const ExpensesPerCategory = ({
   expensesPerCategory,
 }: ExpensesPerCategoryProps) => {
   return (
-    <ScrollArea className="col-span-2 h-full rounded-md border p-6">
+    <ScrollArea className="col-span-2 h-full rounded-md border pb-6">
       <CardHeader>
         <CardTitle className="font-bold">Gastos por Categoria</CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="space-y-6">
         {expensesPerCategory.map((category) => (
           <div key={category.category} className="space-y-2">
             <div className="flex w-full justify-between">
-              <p className="text-sm font-bold">{category.category}</p>
+              <p className="text-sm font-bold">
+                {TRANSACTION_CATEGORY_LABEL[category.category]}
+              </p>
               <p className="text-sm font-bold">{category.percentageOfTotal}%</p>
             </div>
             <Progress value={category.percentageOfTotal} />
